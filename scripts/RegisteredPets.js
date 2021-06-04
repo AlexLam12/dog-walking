4d21255170041a1f1a59b3b97c8531cced7c931d
AlexLam12 <alex.e015@gmail.com>
2021-06-04 14:50:25 -0500

    complete who's walking whom

--- scripts/RegisteredPets.js
+++ scripts/RegisteredPets.js
@@ -1,12 +1,14 @@
 import { getPets } from "./database.js"
+import { getWalkers } from "./database.js"
 
+const walkers = getWalkers()
 const pets = getPets()
 
 export const RegisteredPets = () => {
     let petHTML = "<ul>"
 
     for (const pet of pets) {
-        petHTML += `<li>${pet.name}</li>`
+        petHTML += `<li id="pet--${pet.id}">${pet.name}</li>`
     }
 
     petHTML += "</ul>"
@@ -14,3 +16,29 @@ export const RegisteredPets = () => {
     return petHTML
 }
 
+  
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        console.log(clickEvent)
+        if (itemClicked.id.startsWith("pet")) {
+            const [,petId] = itemClicked.id.split("--")
+
+            for (const pet of pets) {
+                if (pet.id === parseInt(petId)) {
+
+                    let assignedWalker = {name: ""};
+
+                    for (const walker of walkers) {
+                        if (walker.id === parseInt(pet.walkerId)) {
+                            assignedWalker.name = walker.name
+
+                        }
+                    }
+                    window.alert(`${pet.name} is being walked by ${assignedWalker.name}`)
+                }
+            }
+        }
+    }
+)
